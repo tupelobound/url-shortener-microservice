6555c511bdfe50e35d93d84af4599152696b72fe
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-02-13 21:47:45 +0000

    Added mongoose connection and API retrieve route

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-// set up express object
+// set up
+require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
+
+// Connect to MongoDB database
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Create an address model
+const Schema = mongoose.Schema;
+const webAddressSchema = new Schema({
+  url: String,
+  shortUrl: Number
+});
+const WebAddress = mongoose.model('WebAddress', webAddressSchema);
 
 // serve static assets from the 'public' directory
 app.use(express.static('public'))
@@ -10,6 +23,17 @@ app.use(express.static('public'))
 app.get("/", (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
+
+// Serve redirect according to user input
+app.get("/api/shorturl/:number", function(req, res) {
+    WebAddress.find({ shortUrl: req.params.number}, (err, result) => {
+      if (result.length > 0) {
+        res.redirect('http://' + result[0].url);
+      } else {
+        res.sendFile(process.cwd() + '/views/index.html');
+      }
+    })
+  });
 
 // listen on port 3000 for requests
 app.listen(port, () => {
